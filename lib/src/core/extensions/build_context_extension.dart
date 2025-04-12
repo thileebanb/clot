9a633403bc69ec9787d4130e039f495cf0b6321a
Thileeban Balasundaram <thileeban.b@marketsimplified.com>
2025-04-12 15:22:34 +0530

    feat: add build context extension

---
+++ lib/src/core/extensions/build_context_extension.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+extension BuildContextExtension on BuildContext {
+  void hideKeyboard(PointerDownEvent _) => FocusScope.of(this).unfocus();
+
+  Future<T?> push<T extends Object?>(Route<T> route) async {
+    return await Navigator.of(this).push(route);
+  }
+
+  void pop<T extends Object?>([T? result]) async {
+    Navigator.of(this).pop(result);
+  }
+
+  bool canPop() => Navigator.of(this).canPop();
+
+  void popUntil(bool Function(Route<dynamic>) predicate) {
+    Navigator.of(this).popUntil(predicate);
+  }
+
+  Future<T?> pushReplacement<T extends Object?, TO extends Object?>(
+    BuildContext context,
+    Route<T> newRoute, {
+    TO? result,
+  }) async {
+    return await Navigator.pushReplacement(this, newRoute, result: result);
+  }
+
+  Future<T?> pushAndRemoveUntil<T extends Object?>(
+    BuildContext context,
+    Route<T> newRoute,
+    bool Function(Route<dynamic>) predicate,
+  ) async {
+    return await Navigator.pushAndRemoveUntil(this, newRoute, predicate);
+  }
+}
