4570032fc4f623cd2c5c0e13c6cc8c7a062ae546
Thileeban Balasundaram <thileeban.b@marketsimplified.com>
2025-04-12 14:43:46 +0530

    fix: lint issue

--- lib/assets/app_images.dart
+++ lib/assets/app_images.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+// import 'package:flutter_svg/flutter_svg.dart';
 
 abstract class AppImages {
   static final String _assetPath = 'lib/assets/images/';
 
-  static SvgPicture _svgAsset(String fileName) =>
-      SvgPicture.asset('$_assetPath$fileName');
+  // static SvgPicture _svgAsset(String fileName) =>
+  //     SvgPicture.asset('$_assetPath$fileName');
 
   static Image _imgAsset(String fileName) =>
       Image.asset('$_assetPath$fileName');
